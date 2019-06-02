fb967cf22f5ab0647ae033b03929ffea284e59c4
Alex Kotliarskyi <alex.frantic@gmail.com>
2019-06-01 18:31:44 -0700

    Only run when main

--- layout.rb
+++ layout.rb
@@ -24,10 +24,12 @@ class TextLayout
   end
 end
 
-tl = TextLayout.new(21, 8)
-output = tl.render(ARGV.first)
+if __FILE__ == $0
+  tl = TextLayout.new(21, 8)
+  output = tl.render(ARGV.first)
 
-tl.height.times { |row| 
-  from, to = row * tl.width, (row + 1) * tl.width
-  puts "|" + output[from...to] + "|"
-}
+  tl.height.times { |row| 
+    from, to = row * tl.width, (row + 1) * tl.width
+    puts "|" + output[from...to] + "|"
+  }
+end
